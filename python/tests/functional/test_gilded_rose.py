712028f8714dd4981f5dd179ab2d92659e169e50
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2021-06-14 08:09:49 +0200

    feature/conjured test_standard_reduction

--- python/tests/functional/test_gilded_rose.py
+++ python/tests/functional/test_gilded_rose.py
@@ -8,3 +8,13 @@ def test_foo():
     gilded_rose = GildedRose(items)
     gilded_rose.update_quality()
     assert items[0].name == "foo"
+
+
+def test_standard_reduction():
+    """Test that item quality and sell_in is reduce"""
+    items = [Item("foo", 2, 2)]
+    gilded_rose = GildedRose(items)
+    gilded_rose.update_quality()
+    assert items[0].name == "foo"
+    assert items[0].quality == 1
+    assert items[0].sell_in == 1
